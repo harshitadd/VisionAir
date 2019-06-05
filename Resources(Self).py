2a3d66867f6f4c507d47c8c66a875b87e7b14dd0
harshitadd <harshitadd@gmail.com>
2019-06-05 17:08:39 +0530

    datasets for last 6 months

---
+++ Resources(Self).py
@@ -0,0 +1,38 @@
+Machine Learning:
+Intro to ML : Udacity
+Andrew NG ML Course
+Machine Learning using Scikit-Learn and TensorFlow
+
+Deep Learning:
+Andrew Ng Deep Learning
+Deep Learning Udacity
+Deep Learning book with Eel on Cover in Library
+The Deep Learning Book
+
+
+
+
+
+
+
+
+
+To Do : 
+\- Write summary of maam's discussion 
+- Web Scraper - complete understanding 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
