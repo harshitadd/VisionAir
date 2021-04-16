fba54ce592e3ef2e8c2429c882750fac488bb41d
harshitadd <harshitadd@gmail.com>
2021-04-16 09:57:42 +0530

    cleaned

--- datasets/API_weather.py
+++
@@ -1,7 +0,0 @@
-import requests
-import time
-import sched
-
-url = 'http://api.openweathermap.org/data/2.5/weather?id=1261481&APPID=a1a98e4e5f70f02ddb5ea6df28f36a2f&units=metric'
-page = requests.get(url)
-print(page.content)
