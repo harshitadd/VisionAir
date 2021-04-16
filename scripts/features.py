d6022fc2b315ffb3d406a5c9c6dee713ad5fed7d
harshitadd <harshitadd@gmail.com>
2021-04-16 11:02:41 +0530

    Reorganized dataset files

--- scripts/features.py
+++
@@ -1,9 +0,0 @@
-''' 
-    Source : https://www.worldweatheronline.com/developer/premium-api-explorer.aspx
-    API Key :  5ac9d480716d4b7192b43943192106
-'''
-import json 
-
-with open('weather_historic.json') as json_file: 
-    data = json.load(json_file)
-    
